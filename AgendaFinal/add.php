a448ee85e6dd76cc7f4dca2a10fb343edd9832b1
Jhon Wesley <128758538+Jhon-Wesley7@users.noreply.github.com>
2024-06-04 19:40:35 -0300

    bug telefone resolvido
    Estava tendo um bug no telefone mas foi resolvido

--- AgendaFinal/add.php
+++ AgendaFinal/add.php
@@ -1,19 +1,18 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Adicionar Contato</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-    <h1>Adicionar Novo Contato</h1>
-    <form action="save.php" method="POST">
-        <input type="hidden" name="id" value="">
-        <p>Nome: <input type="text" name="name"></p>
-        <p>Endereço: <input type="text" name="address"></p>
-        <p>Telefone:<input type="text" name="phone"></p>
-        <p><input type="submit" value="Salvar"></p>
+    <h1>Adicionar Contato</h1>
+    <form action="save.php" method="post">
+        <label for="name">Nome:</label>
+        <input type="text" id="name" name="name" required><br>
+        <label for="address">Endereço:</label>
+        <input type="text" id="address" name="address" required><br>
+        <label for="phone">Telefone:</label>
+        <input type="text" id="phone" name="phone" required><br>
+        <input type="submit" value="Salvar">
     </form>
 </body>
 </html>
