3e5c379b77c49be00eb09a7a09ba1d0f912b1b0b
Jhon Wesley <128758538+Jhon-Wesley7@users.noreply.github.com>
2024-06-04 19:01:12 -0300

    Update db.php

--- AgendaFinal/db.php
+++ AgendaFinal/db.php
@@ -1,14 +1,15 @@
 <?php
 $servername = "localhost";
-$username = "root";  // Seu nome de usuário MySQL
-$password = "";      // Sua senha MySQL
-$dbname = "agenda_db";
+$username = "root";  // User MySQL
+$password = "";      // Senha MySQL
+$dbname = "agenda_db";  // Nome do Banco
 
-// Cria a conexão
+// Criando a conexão
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Verifica a conexão
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+    die("Connection failed: " . $conn->connect_error); // Atentar para quando criar o banco, criar também as tabelas manualmente.
+} // Vale salientar que acabei quebrando bastante cabeça devido criação apenas do banco de dados, achando que criaria a tabela automaticamente.
 ?>
+
